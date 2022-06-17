fe0475356761d96553ecf0f96fd11afe65c208b5
Laerson Castro <laersonjr@gmail.com>
2022-06-16 23:05:52 -0300

    Codigo comentado como exemplo de configurar o messageSource

---
+++ src/main/java/com/algamoneyapi/config/ExemploMessageSourceCfg.java
@@ -0,0 +1,15 @@
+package com.algamoneyapi.config;
+
+public class ExemploMessageSourceCfg {
+
+/*
+    @Bean(name="messageSource")
+    public ResourceBundleMessageSource bundleMessageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+*/
+
+}
