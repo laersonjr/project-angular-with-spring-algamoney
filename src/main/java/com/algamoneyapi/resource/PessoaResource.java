9571e0472bd04c3be4802e49ac0d065bf5510908
Laerson Castro <laersonjr@gmail.com>
2022-06-18 00:31:18 -0300

    Criado GetMapping e PostMapping de PessoaResource

---
+++ src/main/java/com/algamoneyapi/resource/PessoaResource.java
@@ -0,0 +1,44 @@
+package com.algamoneyapi.resource;
+
+import com.algamoneyapi.model.Pessoa;
+import com.algamoneyapi.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @GetMapping
+    public List<Pessoa> listarPessoas(){
+        return pessoaRepository.findAll();
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo){
+        Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+        return  pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
+        Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(pessoaSalva.getCodigo()).toUri();
+
+        return ResponseEntity.created(uri).body(pessoaSalva);
+    }
+
+}
